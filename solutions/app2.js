2fbd4e711de01a687eb60b9681c00fc39f35613b
barotiv <88926689+barotiv@users.noreply.github.com>
2021-11-12 18:15:56 +0100

    Fulfilled

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,10 @@
+const HU = {
+    date: (hunDate) => new Intl.DateTimeFormat('hu-HU').format(hunDate),
+    curreny: (hunCurrency) => new Intl.NumberFormat('hu-HU', {
+        style: 'currency',
+        currency: 'HUF'
+    }).format(hunCurrency),
+    list: (array) => `${array[0]}, ${array[1]} és ${array[2]}`
+}
 
-
-
-
-
-
-// export default HU
+export default HU
